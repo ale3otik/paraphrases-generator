ad678c9fceb2e0253dc15f7aeddef9a13f1aa043
ale3otik <aleksey.zotov@phystech.edu>
2018-03-18 21:43:56 +0300

    add parameters

---
+++ model/parameters.py
@@ -0,0 +1,27 @@
+from .functional import *
+
+
+class Parameters:
+    def __init__(self, max_word_len, max_seq_len, word_vocab_size, char_vocab_size):
+        self.max_word_len = int(max_word_len)
+        self.max_seq_len = int(max_seq_len) + 1  # go or eos token
+
+        self.word_vocab_size = int(word_vocab_size)
+        self.char_vocab_size = int(char_vocab_size)
+
+        self.word_embed_size = 300
+        self.char_embed_size = 15
+
+        self.kernels = [(1, 25), (2, 50), (3, 75), (4, 100), (5, 125), (6, 150)]
+        self.sum_depth = fold(lambda x, y: x + y, [depth for _, depth in self.kernels], 0)
+
+        self.encoder_rnn_size = 600
+        self.encoder_num_layers = 1
+
+        self.latent_variable_size = 1100
+
+        self.decoder_rnn_size = 800
+        self.decoder_num_layers = 2
+
+        self.kld_penalty_weight = 1.0
+        self.cross_entropy_penalty_weight = 1.0
