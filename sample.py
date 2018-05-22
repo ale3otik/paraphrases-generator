43b1da2242eb5c441e0684c8bac9f4383d48e819
ale3otik <aleksey.zotov@phystech.edu>
2018-05-22 10:17:17 +0300

    fix one more typo

--- sample.py
+++ sample.py
@@ -86,9 +86,9 @@ if __name__ == "__main__":
     sampled_file_dst = 'logs/sampled_out_{}_{}{}.txt'.format(args.input_file,
                                             'mean_' if args.use_mean else '', args.model_name)
     target_file_dst = 'logs/target_out_{}_{}{}.txt'.format(args.input_file,
-                                            'mean_' if args.use_mean else '')
+                                            'mean_' if args.use_mean else '', args.model_name)
     np.save(sampled_file_dst, np.array(result))
-    np.save(target_file_dst, args.model_name), np.array(target))
+    np.save(target_file_dst, np.array(target))
     print('------------------------------')
     print('results saved to: ')
     print(sampled_file_dst)
